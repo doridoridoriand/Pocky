74f4209b2d165db538575f6237ab41eadbdd42c4
Hitoshi Saito <hitoshi.saito@monstar-lab.com>
2015-06-13 20:12:35 +0900

    table追加

--- Pocky/ViewController.swift
+++ Pocky/ViewController.swift
@@ -9,10 +9,40 @@
 import Foundation
 import AppKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+    
+    var dataArray : NSMutableArray = []
+    
+    @IBOutlet weak var tableView: NSTableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 0...10 {
+            let data : NSDictionary = [
+                "title": NSString(format: "title-%d", i),
+                "description": NSString(format: "description-%d", i)
+            ]
+            dataArray.addObject(data)
+        }
+        
+        tableView.target = self
+        tableView.action = "doClick:"
     }
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        let data : NSDictionary = dataArray.objectAtIndex(row) as! NSDictionary
+        return data.objectForKey("title")
+    }
+    
+    func doClick(sender: AnyObject) {
+        NSLog("clickedRow : %d", tableView.clickedRow)
+    }
+    
+    
     
 }
